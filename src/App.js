a87de0968c3fb9c331c0a5443f6a1fbbe6f79edd
Eren Elmacı <erenx1623@gmail.com>
2023-03-11 02:20:52 +0300

    dotenv complated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
+import AuthContext from "./context/AuthContext"
 import Router from "./router/Router"
 
 function App() {
   return (
     <div className="App">
       <Router />
+      <AuthContext />
     </div>
   )
 }
