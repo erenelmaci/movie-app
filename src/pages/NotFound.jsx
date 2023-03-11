62e06f7e1568c9fb2ebad5a78ed1fe306dabb581
Eren Elmacı <erenx1623@gmail.com>
2023-03-11 15:26:28 +0300

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import errorImage from "../assets/error-500_aa3dde6a.jpg"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <>
+      <div>
+        <div>
+          <img src={errorImage} alt="" />
+        </div>
+        <div>
+          <h1>SORRY, THE PAGE YOU REQUESTED IS NOT FOUND!</h1>
+          <p>
+            I think you're in the wrong place because I can't see the page
+            you're looking for.
+          </p>
+          <button>BACK TO HOME PAGE</button>
+        </div>
+      </div>
+    </>
   )
 }
 
