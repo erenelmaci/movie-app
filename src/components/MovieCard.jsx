e66db9a539bc325d5f7f59f4603e1b396de390e1
Eren Elmacı <erenx1623@gmail.com>
2023-03-11 14:32:51 +0300

    update

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,70 +1,21 @@
 import "../styles/MovieCard.css"
-import axios from "axios"
-import { useState, useEffect } from "react"
-import { Row, Col } from "react-bootstrap"
-import Form from "react-bootstrap/Form"
-import { Button } from "react-bootstrap"
-import { Link } from "react-router-dom"
 
-const MovieCard = () => {
-  const [poster, setPoster] = useState(null)
-  const [plot, setPlot] = useState(null)
-  const [title, setTitle] = useState(null)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const OMDB_API_URL = `https://www.omdbapi.com/?i=tt3896198&apikey=baf219d`
-      try {
-        const response = await axios.get(OMDB_API_URL)
-        setPoster(response.data.Poster)
-        setPlot(response.data.Plot)
-        setTitle(response.data.Title)
-        console.log(response.data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchData()
-  }, [])
-
+const MovieCard = ({ poster, overview, title }) => {
   return (
     <>
-      <div
-        style={{ height: "5rem" }}
-        className="d-flex justify-content-center mt-4"
-      >
-        <Form.Control
-          style={{ height: "2.5rem" }}
-          className="w-25 d-flex justify-center allign-center bg-dark text-white"
-          type="text"
-          placeholder="Readonly input here..."
-        />
-        <Button style={{ height: "2.5rem" }} variant="warning">
-          Search
-        </Button>
+      <div className="book">
+        <div className="comment">
+          <h4>{title}</h4>
+          <p>{overview}</p>
+        </div>
+        <div className="cover">
+          <img
+            className="posters-img"
+            src={`https://image.tmdb.org/t/p/w780/${poster}`}
+            alt="img"
+          />
+        </div>
       </div>
-
-      <Link to="/movie-detail">
-        <Row className="d-flex justify-content-center w-100">
-          <Col
-            className="d-flex justify-content-center"
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-          >
-            <div className="book">
-              <div className="comment">
-                <h4>{title}</h4>
-                <p>{plot}</p>
-              </div>
-              <div className="cover">
-                <img className="posters-img" src={poster} alt="img" />
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Link>
     </>
   )
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,64 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Col, Row } from "react-bootstrap"
 import MovieCard from "../components/MovieCard"
+import { Button } from "react-bootstrap"
+import Form from "react-bootstrap/Form"
+import { Link } from "react-router-dom"
 
 const Main = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const theMovieApiKey = process.env.REACT_APP_THEMOVIE_API_KEY
+      const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${theMovieApiKey}`
+      try {
+        const response = await axios.get(URL)
+        setMovies(response.data.results)
+        console.log(response.data.results)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <>
-      <MovieCard />
+      <div
+        style={{ height: "5rem" }}
+        className="d-flex justify-content-center mt-4"
+      >
+        <Form.Control
+          style={{ height: "2.5rem" }}
+          className="w-25 d-flex justify-center allign-center bg-dark text-white"
+          type="text"
+          placeholder="Readonly input here..."
+        />
+        <Button style={{ height: "2.5rem" }} variant="warning">
+          Search
+        </Button>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <Row md={6} lg={4} xl={12} className="g-4">
+          {movies.map((movie) => (
+            <Link key={movie.id} to={`/movie-detail/${movie.id}`}>
+              <Col
+                className="d-flex justify-content-center align-items-center"
+                key={movie.id}
+              >
+                <MovieCard
+                  poster={movie.poster_path}
+                  overview={movie.overview}
+                  title={movie.title}
+                  id={movie.id}
+                />
+              </Col>
+            </Link>
+          ))}
+        </Row>
+      </div>
     </>
   )
 }
